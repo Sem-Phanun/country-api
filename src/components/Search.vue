95d863eefe9a806c4aba924cb4e354dd1738aa34
DESKTOP-J9R2688\Bright <phanunsem@gmail.com>
2024-05-01 22:38:32 +0700

    implement fuzzy search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,14 +1,13 @@
 <template>
-    <div class="py-4">
-            <form class="flex justify-center items-center">
-                <label class="text-lg font-bold mb-2 mx-2" for="search-input">Search:</label>
-                <input type="search" id="search-input" class="p-2 pl-10 w-[20rem] text-lg border border-gray-400 rounded focus:outline-none">
-                <button class="mx-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Search</button>
-            </form>
-    </div>
+    
 </template>
 
 <script setup>
+const props = defineProps({
+    searchQuery: {
+        type: String,
+    }
+})
 
 </script>
 
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,9 +1,5 @@
 <template>
     <div class="text-slate-800">
-        <div>
-            <h1 class="text-center text-4xl">List of country</h1>
-        </div>
-        <Search/>
         <CountryCard/>
     </div>
 </template>
@@ -11,7 +7,6 @@
 <script setup>
 import Search from '@/components/Search.vue'
 import CountryCard from '@/components/CountryCard.vue';
-
 
 </script>
 
